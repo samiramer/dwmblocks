ffaff9dcec53a19c3ac111dc1d269e2918087ccd
samer <samirmamer@gmail.com>
2021-11-14 19:16:45 -0500

    Add my custom status commands

--- blocks.def.h
+++ blocks.def.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+  {"", "dwm_vpnstatus", 5, 0},
+  {"", "dwm_pomo", 1, 0},
+  {"墳 ", "dwm_volume", 0, 10},
+  {" ", "brightnessctl| sed -n '2 p'|awk '{print substr($4, 2, length($4) - 2)}'", 0, 11},
+	{" ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 5, 0},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{"", "date '+%b %d %I:%M:%S %p'", 1, 0},
+  {"", "dwm_batterystatus", 5, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
