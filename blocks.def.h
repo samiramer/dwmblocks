314b42352ac8428edaf35b702e419656879b6e28
Samir Amer <samir@sunnybyte.com>
2024-03-15 02:34:27 -0400

    Remove icons

--- blocks.def.h
+++ blocks.def.h
@@ -3,11 +3,11 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 /*  {"", "dwm_vpnstatus", 5, 0},*/
 /*  {"", "dwm_pomo", 1, 0},*/
-  {" ", "dwm_volume", 0, 10},
+  {" ", "dwm_volume", 0, 10},
   {"", "vpn_status", 5, 0},
-	{"󰍛 ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 5, 0},
+	{" ", "free -h | awk '/^Mem/ { print $3 }' | sed s/i//g", 5, 0},
 
-	{"󱑍 ", "date '+%b %d %I:%M:%S %p'", 1, 0},
+	{"", "date '+%b %d %I:%M:%S %p'", 1, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
